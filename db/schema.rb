f7df504530f35f17670d7acb67d00af69c65c8de
Brandon Teich <brandonteich123@gmail.com>
2019-09-15 22:53:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 
 ActiveRecord::Schema.define(version: 0) do
   create_table "coupons", force: :cascade do |t|
-    t.string "coupon_code"
-    t.string "store"
-  end
+  t.string "coupon_code"
+  t.string "store"
+end
 
 end
