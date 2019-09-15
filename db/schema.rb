9eaeee2074efd51fc1d16ca41dfc3a81f412ba18
Brandon Teich <brandonteich123@gmail.com>
2019-09-15 22:52:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,5 +12,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+  create_table "coupons", force: :cascade do |t|
+    t.string "coupon_code"
+    t.string "store"
+  end
 
 end
